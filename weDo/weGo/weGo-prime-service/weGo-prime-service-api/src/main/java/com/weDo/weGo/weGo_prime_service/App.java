39759d8059cd01ba2f0225d7c05a26468bee988c
MarshallJin <marshal.jin@outlook.com>
2016-04-13 17:45:37 +0800

    com.wedo.wego
    wego app

---
+++ weDo/weGo/weGo-prime-service/weGo-prime-service-api/src/main/java/com/weDo/weGo/weGo_prime_service/App.java
@@ -0,0 +1,13 @@
+package com.weDo.weGo.weGo_prime_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
